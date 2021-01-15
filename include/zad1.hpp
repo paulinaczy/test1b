baea3c302d0520f87bed024c29db70d49f87883c
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2021-01-15 08:46:48 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,22 @@
 #include <string>
 
 // tutaj klasa Artysta
+using namespace std;
+
+class Artysta {
+public:
+	Artysta():pseudonim("Anonim"), sluchacze(0){}
+	Artysta(string x, unsigned y): pseudonim(x), sluchacze(y){}
+	virtual void graj(ostream& o) const {
+		o << pseudonim << ": " << sluchacze << endl;
+	}
+	const string getPseudonim() const {
+		return pseudonim;
+	}
+	unsigned getSluchacze() const {
+		return sluchacze;
+	}
+private:
+	string pseudonim;
+	unsigned sluchacze;
+};
